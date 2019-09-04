3d5e504e50b2134138e688cdf8e18e84fb72efd6
jixingyu <ymx2011@gmail.com>
2019-09-04 15:18:28 +0800

    init

---
+++ src/plugin.js
@@ -0,0 +1,73 @@
+import videojs from 'video.js';
+import {version as VERSION} from '../package.json';
+
+const Plugin = videojs.getPlugin('plugin');
+
+// Default options for the plugin.
+const defaults = {
+  format: '观看完整视频请登录%s',
+  login: '',
+  register: '',
+};
+
+const setupLogin = (player, options) => {
+  if (options.format !== '' && options.login !== '') {
+    const videoEl = player.el();
+    const div = document.createElement('div');
+  
+    div.classList.add('vjs-login-content');
+  
+    div.innerHTML = options.format.replace(/%s/, '<a href="">' + options.login + '</a>');
+    videoEl.appendChild(div);
+  }
+
+  if (options.login !== '') {
+    player.on('ended', function() {
+      // show login & register
+    });
+  }
+};
+
+/**
+ * An advanced Video.js plugin. For more information on the API
+ *
+ * See: https://blog.videojs.com/feature-spotlight-advanced-plugins/
+ */
+class Login extends Plugin {
+
+  /**
+   * Create a Login plugin instance.
+   *
+   * @param  {Player} player
+   *         A Video.js Player instance.
+   *
+   * @param  {Object} [options]
+   *         An optional options object.
+   *
+   *         While not a core part of the Video.js plugin architecture, a
+   *         second argument of options is a convenient way to accept inputs
+   *         from your plugin's caller.
+   */
+  constructor(player, options) {
+    // the parent class will add player under this.player
+    super(player);
+
+    this.options = videojs.mergeOptions(defaults, options);
+
+    this.player.ready(() => {
+      this.player.addClass('vjs-login');
+      setupLogin(player, this.options)
+    });
+  }
+}
+
+// Define default values for the plugin's `state` object here.
+Login.defaultState = {};
+
+// Include the version number.
+Login.VERSION = VERSION;
+
+// Register the plugin with video.js.
+videojs.registerPlugin('login', Login);
+
+export default Login;
